f3f311f2b1c3fd7bea7c81cb67314462bde31c1f
Muneer Hashmat <muneerhashmat@gmail.com>
2024-05-21 20:11:48 +0530

    added filter by category in Foods.jsx

--- src/components/Food/Foods.jsx
+++ src/components/Food/Foods.jsx
@@ -3,12 +3,15 @@ import Footer from "../Footer";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { FadeLoader } from "react-spinners";
+import AdsClickIcon from "@mui/icons-material/AdsClick";
 
 const Food = () => {
   const [currentDishes, setCurrentDishes] = useState([]);
   const [countries, setCountries] = useState([]);
+  const [categories, setCategories] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [inputCountry, setInputCountry] = useState("");
+  const [inputCategory, setInputCategory] = useState("");
 
   const fetchInitialDishes = async () => {
     try {
@@ -33,9 +36,22 @@ const Food = () => {
     }
   };
 
+  const fetchCategories = async () => {
+    try {
+      const response = await axios(
+        "https://www.themealdb.com/api/json/v1/1/categories.php"
+      );
+      console.log(response);
+      setCategories(response.data.categories);
+    } catch (e) {
+      console.log("An error occured: " + e);
+    }
+  };
+
   useEffect(() => {
     fetchInitialDishes();
     fetchCountries();
+    fetchCategories();
   }, []);
 
   const searchDish = async () => {
@@ -74,21 +90,39 @@ const Food = () => {
 
   useEffect(() => {
     setCurrentDishes([]);
-    setTimeout(() => {
-      searchByCountry();
-    }, 2000);
+    searchByCountry();
   }, [inputCountry]);
+
+  const searchByCategory = async () => {
+    let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+    console.log(inputCategory);
+    if (inputCategory.length > 0) {
+      url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${inputCategory}`;
+    }
+    try {
+      const response = await axios(url);
+      console.log(response);
+      setCurrentDishes(response.data.meals);
+    } catch (e) {
+      console.log("An error occured: " + e);
+    }
+  };
+
+  useEffect(() => {
+    setCurrentDishes([]);
+    searchByCategory();
+  }, [inputCategory]);
 
   return (
     <>
       <Navbar />
       <div className=" pt-28">
-        <div className="w-[600px] mx-auto mb-3">
+        <div className="w-[700px] mx-auto mb-3">
           <h1 className="text-2xl text-center font-bold">
             Search for your desired recipe
           </h1>
 
-          <div className="flex justify-between items-center mt-3">
+          <div className="flex justify-between items-center flex-wrap mt-3">
             <input
               type="text"
               placeholder="search for a dish"
@@ -101,10 +135,23 @@ const Food = () => {
               onChange={(e) => setInputCountry(e.target.value)}
               className="p-2 text-xl outline-none border-2 border-gray-400 rounded-md "
             >
-              <option value="">All Countries</option>
+              <option value="">By Country</option>
               {countries.map((country, index) => (
                 <option key={index} value={country.strArea}>
                   {country.strArea}
+                </option>
+              ))}
+            </select>
+
+            <select
+              value={inputCategory}
+              onChange={(e) => setInputCategory(e.target.value)}
+              className="p-2 text-xl outline-none border-2 border-gray-400 rounded-md "
+            >
+              <option value="">By Category</option>
+              {categories.map((category, index) => (
+                <option key={index} value={category.strCategory}>
+                  {category.strCategory}
                 </option>
               ))}
             </select>
@@ -127,17 +174,19 @@ const Food = () => {
                   className="w-full h-[200px] object-cover"
                 />
 
-                <div className="px-4 py-4 flex justify-between items-center">
+                <div className="px-4 py-4 flex justify-between items-start">
                   <div className="flex flex-col gap-1">
-                    <p className="text-xl font-bold">{dish.strMeal}</p>
+                    <p className="text-xl font-bold w-[230px]">
+                      {dish.strMeal}
+                    </p>
                     <p className="text-yellow-600">{dish.strArea}</p>
                     <p className="text-sm text-gray-600 font-semibold">
-                      Category: {dish.strCategory}
+                      {dish.strCategory}
                     </p>
                   </div>
 
-                  <button className="font-bold p-2 rounded-md bg-blue-500 text-white hover:scale-110 transition-all">
-                    click for Recipe
+                  <button className="font-bold p-2 rounded-md bg-blue-500 text-white hover:scale-110 transition-all w-[120px]">
+                    <AdsClickIcon /> Recipe
                   </button>
                 </div>
               </div>
